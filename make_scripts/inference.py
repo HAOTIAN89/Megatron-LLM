3f7fec31872d7f8ae08b6fa02644fcab7984f025
HAOTIAN89 <1285770300@qq.com>
2023-12-15 10:06:14 +0000

    write one simple but effective inference codes

--- make_scripts/inference.py
+++ make_scripts/inference.py
@@ -1,18 +1,28 @@
 import torch
-import transformers
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=LlamaForCausalLM.from_pretrained("/pure-mlo-scratch/make_project/trial-runs/meditron-7b-summarizer/hf_checkpoint/"),
-    tokenizer=LlamaTokenizer.from_pretrained("/pure-mlo-scratch/make_project/trial-runs/meditron-7b-summarizer/hf_checkpoint/"),
-    torch_dtype=torch.bfloat16,
-    device="cuda"
-)
-prompt = """Given the provided patient-doctor dialogue, write the corresponding patient information summary in JSON format.\nMake sure to extract all the information from the dialogue into the template, but do not add any new information. \nIf a field is not mentioned, simply write \"feature\": \"None\".\n\nDoctor: Hello, how are you today?\nPatient: I'm fine, thank you.\nDoctor: I understand you are here to see me about some skin lesions you have been experiencing. Can you tell me more about it?\nPatient: Yes, I have been suffering from multiple pigmented annular papules and plaques on my face for 7 years.\nDoctor: Hmm, I see. And have these lesions been slowly progressive?\nPatient: Yes, that's correct.\nDoctor: Have you noticed any signs of self-healing?\nPatient: No, I haven't.\nDoctor: Alright. Can you tell me a bit about your physical and medical history?\nPatient: I was a normal, healthy, full-term baby and had normal physical milestones of development as a child. I have no history of recurrent infections or any other skin lesions. My parents and siblings are all normal and healthy. And I have no family history of a similar condition or other skin problems.\nDoctor: Okay, that's good to know. Can you describe the examination you had?\nPatient: The central part of my face, i.e. the malar area and nose including the ala nasi, showed scattered annular papules and plaques of different sizes ranging from 0.3 to 2 cm. The lesions were asymptomatic and symmetrically distributed on both sides of my face.\nDoctor: I see. And have you had any laboratory tests done?\nPatient: Yes, I have. The results were within normal ranges.\nDoctor: Alright. And have you had any biopsies taken from the lesions?\nPatient: Yes, multiple biopsies were taken from different lesions.\nDoctor: I see. And what was the diagnosis confirmed as?\nPatient: The diagnosis was confirmed as DSAP.\nDoctor: Okay, I understand. And what kind of treatment options were discussed?\nPatient: All physical modalities were excluded to avoid the possibility of side effects like scarring and disfigurement, as the lesions were located in the central part of my face and involved the skin overlying the nasal cartilage. We first prescribed a topical retinoic acid 0.1% cream that has keratolytic and antineoplastic effects.\nDoctor: I see. And how did the patient respond to the treatment?\nPatient: I applied the cream twice daily for 3 months without a significant response.\nDoctor: I see. And when did you present again?\nPatient: I presented again six months later.\nDoctor: And what was the picture like at that time?\nPatient: The picture was almost unchanged except for a few small changes.\nDoctor: Okay, I understand. I will have to examine you again and see if there are any other treatment options we can consider.
-"""
+model_id = "/pure-mlo-scratch/make_project/trial-runs/meditron-7b-summarizer/hf_checkpoint/"
 
-sequences = pipeline(prompt, max_new_tokens=400, do_sample=True, top_k=20, num_return_sequences=1)
+# load model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(model_id, use_cache=True, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_cache=False)
 
-for sequence in sequences:
-    print(sequence['generated_text'])
+# Set the prompt
+prompt = "Given the provided patient-doctor dialogue, write the corresponding patient information summary in JSON format.\nMake sure to extract all the information from the dialogue into the template, but do not add any new information. \nIf a field is not mentioned, simply write \"feature\": \"None\".\n\nDoctor: Good morning, sir. How are you feeling today?\nPatient: Not so good, doctor. I'm in a lot of pain.\nDoctor: I understand. Can you tell me what happened to you?\nPatient: I was rescued after being jammed between an excavator basket and a wall with some stacked material for 5 minutes.\nDoctor: I see. When you arrived at the emergency room, what were your vital signs like?\nPatient: I think my blood pressure was 121\/71 mmHg, heart rate was 90 beats per minute, respiratory rate was 30 breaths per minute, body temperature was 36.9\u00b0C, and O2 saturation was 94%.\nDoctor: Okay. And what did you complain of when you arrived?\nPatient: I complained of severe pain on the whole left side of my body down to the waist and on the right chest wall.\nDoctor: I see. We performed a plain radiography and a computed tomography on you in the emergency room. The results showed that you have multiple rib fractures on your right 7th and 8th ribs and your left 3rd to 12th ribs, as well as a hemothorax on both sides with the left side being dominant, a left scapular fracture, a liver laceration, a retro-peritoneal hematoma, and a transverse process fracture of your thoracic and lumbar spine.\nPatient: Hmm. That's a lot of injuries.\nDoctor: Yes, it is. You were initially admitted to the intensive care unit after a closed thoracostomy. There was no special complication during the chest tube insertion, and the initial drainage was approximately 200 mL.\nPatient: Okay.\nDoctor: There was bloody drainage during the chest tube insertion, but it became serous and you were transferred to the general ward the next day. On the 4th post-trauma day, you complained of acute pain radiating in the left rear direction. We found about 400 mL of bloody drainage at the same time.\nPatient: Yes, I remember that.\nDoctor: When you complained of pain, you became drowsy as your blood pressure decreased to 70\/50 mmHg. However, your blood pressure became 100\/70 mmHg after the infusion of 300 mL of isotonic saline. Although your vital signs became stable with a heart rate of 71 beats per minute, respiratory rate of 24 breaths per minute, body temperature of 36.6\u00b0C, and O2 saturation of 99%, bloody drainage of 100 mL or more hourly continued.\nPatient: That's concerning.\nDoctor: Yes, it is. We will need to monitor your condition closely and continue to provide you with appropriate treatment.\nPatient: Okay.\nDoctor: Is there anything else you would like to ask me, sir?\nPatient: No, I think that's all for now.\nDoctor: Alright, please let me know if you have any questions or concerns in the future.\nPatient: I will, thank you, doctor."
+
+# use a pipeline to generate text
+pipe = pipeline("text-generation",
+                model=model,
+                tokenizer= tokenizer,    
+                max_new_tokens = 200,
+                do_sample=True,
+                top_k=10,
+                num_return_sequences=2,
+                eos_token_id=tokenizer.eos_token_id,
+                return_full_text=False
+                )
+
+# generate from prompt
+generated = pipe(prompt)
+print(generated['generated_text'])
