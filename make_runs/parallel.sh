d9dae899c58cf2847289b728da31601613034ec9
HAOTIAN89 <1285770300@qq.com>
2024-03-30 12:55:02 +0000

    update the model converting and inference section

--- make_runs/parallel.sh
+++ make_runs/parallel.sh
@@ -5,6 +5,7 @@ echo "Init"
 # python ../make_scripts/parallelize.py /pure-mlo-scratch/make_project/trial-runs/meditron-7b-direct/llama-2-7b-tp4-pp1-meditron-direct/ --tp=1 --pp=1 --vocab=32019 --it=$1
 # python ../make_scripts/parallelize.py /pure-mlo-scratch/make_project/trial-runs/meditron-7b-direct-trunc/llama-2-7b-tp4-pp1-meditron-direct-trunc/ --tp=1 --pp=1 --vocab=32019 --it=$1
 # python ../make_scripts/parallelize.py /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-summarizer/llama-2-13b-tp8-pp1-ex1pubmedreplay-summarizer/ --tp=1 --pp=1 --vocab=32019 --it=$1
-python ../make_scripts/parallelize.py /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-generator/llama-2-13b-tp8-pp1-ex1pubmedreplay-generator1/ --tp=1 --pp=1 --vocab=32019 --it=$1
+# python ../make_scripts/parallelize.py /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-generator/llama-2-13b-tp8-pp1-ex1pubmedreplay-generator1/ --tp=1 --pp=1 --vocab=32019 --it=$1
 # python ../make_scripts/parallelize.py /pure-mlo-scratch/make_project/trial-runs/pubmed-13b-direct-trunc/llama-2-13b-tp8-pp1-ex1pubmedreplay-direct-trunc/ --tp=1 --pp=1 --vocab=32019 --it=$1
+python ../make_scripts/parallelize.py /pure-mlo-scratch/make_project/spring2024/trial-runs/meditron-7B-BHC/llama-2-7b-tp4-pp1-meditron-bhc-2k/ --tp=1 --pp=1 --vocab=32019 --it=$1
 echo "End"
